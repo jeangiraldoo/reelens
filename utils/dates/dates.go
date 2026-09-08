ec5331df84637609d2b53ffd960a03ae6dfe7c90
Jean Giraldo <jeann.giraldo@gmail.com>
2026-09-08 09:37:22 -0500

    refactor(ls): improve command output
    - Renames `Published date` column to `Published`
    - Renames `Cached in` column to `Last checked`
    - Removes the `Source` column

--- utils/dates/dates.go
+++ utils/dates/dates.go
@@ -1,16 +1,53 @@
 package dateUtils
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-var dateFormat = "Jan 2, 2006"
+const (
+	dateFormat   = "Jan 2, 2006"
+	hoursPerDay  = 24
+	hoursPerWeek = hoursPerDay * 7
+)
 
-// Converts an RFC3339 timestamp into a short human-friendly date.
-// Values that fail to parse are returned unchanged, so legacy or
-// unexpected data degrades visibly instead of vanishing.
-func Humanize(stored string) string {
-	t, err := time.Parse(time.RFC3339, stored)
+func HumanTimeSince(date string) string {
+	t, err := time.Parse(time.RFC3339, date)
 	if err != nil {
-		return stored
+		return date
 	}
-	return t.Format(dateFormat)
+
+	timeSince := time.Since(t)
+
+	if timeSince > hoursPerWeek*time.Hour {
+		return t.Format(dateFormat)
+	}
+
+	units, unitName := GetTimeUnit(t)
+
+	if units > 1 { // Every number greater that 1 uses the plural form of the unit
+		unitName += "s"
+	}
+
+	return fmt.Sprintf("%d %s ago", int(units), unitName)
+}
+
+func GetTimeUnit(t time.Time) (float64, string) {
+	timeSince := time.Since(t)
+
+	if timeSince < time.Minute {
+		return timeSince.Seconds(), "second"
+	}
+
+	if timeSince < time.Hour {
+		return timeSince.Minutes(), "minute"
+	}
+
+	hours := timeSince.Hours()
+
+	if timeSince < hoursPerDay*time.Hour {
+		return hours, "hour"
+	}
+
+	return hours / hoursPerDay, "day"
 }
